f8f0480ce7287e23a79040e78c7fd87050c7294b
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-05-31 16:57:07 +0100

    Route calls JAR executable

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var exec = require('child_process').exec;
 
 router.get('/getBasicInfo', function(req, res ,next){
-    console.log(req.query.handle);
+    exec('java -jar java/TweetTrack.jar ' + req.query.handle, function(err, stdout) {
+        if(err) res.send(err);
+        else {
+            console.log(stdout);
+        }
+    });
 });
 
 module.exports = router;
