4ce47f30002f0bf6e8af9346308069c2336b8ee9
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-06-01 17:53:05 +0100

    Add method called when add button is clicked

--- public/javascripts/accountDisplay.js
+++ public/javascripts/accountDisplay.js
@@ -6,3 +6,17 @@ var tableRowHTML = [
     "</td>",
     "</tr>"
 ];
+
+var invalidHandleChars = ['{', '}'];
+
+// {{!-- <tr>
+//     <th scope="row">Donald J. Trump</th>
+//     <td>@realDonaldTrump</td>
+//     <td>42.1K</td>
+//     <td>60.6M</td>
+//   </tr> --}}
+
+$('#add-account-btn').click(function(event){
+    var handle = $('#handle-input').val();
+    console.log(handle);
+});
