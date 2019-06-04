7a889fb74c128537c380ccf2c45b736e9e01bd46
ethanpadden <danu7_pe@danu7>
2019-06-04 11:32:45 +0100

    Add function to check handle on key up and button click

--- public/javascripts/accountDisplay.js
+++ public/javascripts/accountDisplay.js
@@ -1,44 +1,59 @@
-var invalidHandleChars = ['{', '}'];
-
-
-    
-
+$('#handle-input').keyup(function(event){
+    var handle = event.target.value;
+    isHandleValid(handle);
+});
 $('#add-account-btn').click(function(event){
     var handle = $('#handle-input').val();
     if (isHandleValid(handle)) {
-        $.ajax({
-            type: 'GET',
-            url: '/account/getBasicInfo',
-            data: {'handle':handle},
-            success: function(data){
-                console.log(data);
-                var tableRow = '<tr>'
-                + '<th scope="row">' + data.name + '</th>'
-                + '<td>@' + data.handle + '</td>'
-                + '<td>' + data.tweetCount + '</td>'
-                + '<td>' + data.followersCount + '</td>'
-                + '</tr>';
-                var tableHTML = $('#overview-table-body').html();
-                tableHTML += tableRow;
-                $('#overview-table-body').html(tableHTML);
-            },
-            error: function(errMsg) {
-                console.log(errMsg);
-            }
-        });
+        console.log("Yes");
+    //     // $.ajax({
+    //     //     type: 'GET',
+    //     //     url: '/account/getBasicInfo',
+    //     //     data: {'handle':handle},
+    //     //     success: function(data){
+    //     //         console.log(data);
+    //     //         var tableRow = '<tr>'
+    //     //         + '<th scope="row">' + data.name + '</th>'
+    //     //         + '<td>@' + data.handle + '</td>'
+    //     //         + '<td>' + data.tweetCount + '</td>'
+    //     //         + '<td>' + data.followersCount + '</td>'
+    //     //         + '</tr>';
+    //     //         var tableHTML = $('#overview-table-body').html();
+    //     //         tableHTML += tableRow;
+    //     //         $('#overview-table-body').html(tableHTML);
+    //     //     },
+    //     //     error: function(errMsg) {
+    //     //         console.log(errMsg);
+    //     //     }
+    //     // });
     } else {
-        var errMsg = "Please enter valid Twitter handle. Twitter handles must not contain the following characters: ";
-        for(i in invalidHandleChars) {
-            errMsg += invalidHandleChars[i] + ' ';
-        }
-        alert(errMsg);
+    //     // var errMsg = "Please enter valid Twitter handle. Twitter handles must not contain the following characters: ";
+    //     // for(i in invalidHandleChars) {
+    //     //     errMsg += invalidHandleChars[i] + ' ';
+    //     // }
+    //     // alert(errMsg);
+    //     console.log("No");
     }
+    // isHandleValid(handle);
 });
 
+var reg = /a/;
+// var reg1 = new Regex('a');
+
+
+
 function isHandleValid(handle) {
-    var i;
-    for(i in invalidHandleChars) {
-        if(handle.includes(invalidHandleChars[i])) return false;
+    var specialCharReg =  /^[A-Za-z0-9_]{1,15}$/;
+
+    if(handle.length == 0) {
+        console.log("Please enter a handle");
+        return false;
+    } if(handle.length > 15) {
+        console.log("Handle cannot be more than 15 characters long");
+        return false;
+    } else if (!specialCharReg.test(handle)){
+        console.log("Handles can only contain alphanumeric characters and underscores");
+        return false;
     }
 
     return true;
