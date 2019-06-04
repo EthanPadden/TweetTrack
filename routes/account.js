24a0135a33886d47e4eaa57c7dcf08201c9b4fec
ethanpadden <danu7_pe@danu7>
2019-06-04 12:10:32 +0100

    Add status attribute to JSON returned from route

--- routes/account.js
+++ routes/account.js
@@ -3,18 +3,16 @@ var router = express.Router();
 var exec = require('child_process').exec;
 
 router.get('/getBasicInfo', function(req, res ,next){
-    console.log(req.query.handle);
     exec('java -jar java/TweetTrack.jar ' + req.query.handle, function(err, stdout) {
-        if(err) res.send(err);
+        if(err) {
+            res.json({"status":-1});
+        }
         else {
             if(stdout) {
                 var components = stdout.split('{');
                 var outputString = '{' + components[components.length-1];
                 var outputJSON = JSON.parse(outputString);
-                console.log(outputString);
-                if(outputJSON.handle == req.query.handle) 
-                    outputJSON.exactMatch = "true";
-                 else outputJSON.exactMatch = "false";
+                outputJSON.status = 0;
                 res.json(outputJSON);
             }
         }
