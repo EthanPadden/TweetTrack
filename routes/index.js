9e72b4059aa9d999ebb4201be76221431f2fa000
ethanpadden <danu7_pe@danu7>
2019-06-11 16:02:28 +0100

    Change home page route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 /* GET overview dashboard */
-router.get('/overview', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('dash_o', { title: 'Overview' });
 });
 
