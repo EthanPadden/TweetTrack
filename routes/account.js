ab039f2b1d705b93cdfb3aef37c1b3133c77c1ee
ethanpadden <danu7_pe@danu7>
2019-06-12 14:02:39 +0100

    Use spawn to solve max buffer problem

--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var exec = require('child_process').exec;
+const { spawn } = require('child_process');
 var separator = '|||';
 
 router.get('/getBasicInfo', function(req, res ,next){
     // Input data: { handle:string }
-    exec('java -jar java/TweetTrack.jar overview ' + req.query.handle, function(err, stdout) {
+    spawn('java -jar java/TweetTrack.jar overview ' + req.query.handle, function(err, stdout) {
         if(err) res.json({"status":-1});
         else {
             if(stdout) {
@@ -20,23 +20,34 @@ router.get('/getBasicInfo', function(req, res ,next){
 });
 
 router.get('/getTweetInfo', function(req, res, next){
-    // Input data: { handle:string, count:int}
-    exec('java -jar java/TweetTrack.jar tweetstats ' + req.query.handle + ' ' + req.query.count, {maxBuffer: 1024 * 500}, function(err, stdout) {
-        if(err) {
-            console.log(err);
-            res.json({"status":-1});
-        }
-        else {
-            if(stdout) {
-                var components = stdout.split(separator);
-                var tweetData = components[components.length-1];
-                var tweetJSON = JSON.parse(tweetData);
-                tweetJSON.status = 0;
-                res.json(tweetJSON);
-            }
-        }
+        // Input data: { handle:string, count:int}
+    // var child = spawn('java -jar java/TweetTrack.jar tweetstats ' + req.query.handle + ' ' + req.query.count);
+    const child = spawn('java', ['-jar', 'java/TweetTrack.jar', 'tweetstats', req.query.handle, req.query.count]);
+    // var child = spawn('ls');
+
+    child.on('exit', function (code, signal, error) {
+        console.log(code);
+        console.log(signal);
+        console.log(error);
+        // var components = stdout.split(separator);
+        // var tweetData = components[components.length-1];
+        // var tweetJSON = JSON.parse(tweetData);
+        // tweetJSON.status = 0;
+        // res.json(tweetJSON);
+
     });
 
+    child.stdout.on('data', (data) => {
+        console.log(`child stdout:\n${data}`);
+      });
+      
+      child.stderr.on('data', (data) => {
+        console.error(`child stderr:\n${data}`);
+      });
+      child.on('error', function(err) {
+        console.log('Error: ' + err);
+      });     
+    
 });
 
 module.exports = router;
