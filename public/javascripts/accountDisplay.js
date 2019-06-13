1e54ec6bc2e7506ef8c649e1a239859202e666a5
ethanpadden <danu7_pe@danu7>
2019-06-13 16:31:28 +0100

    Display correct information for each tab

--- public/javascripts/accountDisplay.js
+++ public/javascripts/accountDisplay.js
@@ -231,8 +231,27 @@ function isHandleValid(handle) {
  $('#graph-tab').click(function(event){
     $(event.target).addClass('active');
     $('#compare-tab').removeClass('active');
+    showGraphs();
 });
  $('#compare-tab').click(function(event){
     $(event.target).addClass('active');
     $('#graph-tab').removeClass('active');
+    hideGraphs();
+
 });
+
+function hideGraphs() {
+    $('#likesChart').addClass('hidden');
+    $('#RTsChart').addClass('hidden');
+    $('#graph-options').addClass('hidden');
+}
+function showGraphs() {
+    $('#graph-options').removeClass('hidden');
+    
+
+if ( $('#likesChart canvas').hasClass('chartjs-render-monitor'))   $('#likesChart').removeClass('hidden');
+if ( $('#RTsChart canvas').hasClass('chartjs-render-monitor'))   $('#RTsChart').removeClass('hidden');
+
+
+}
+
