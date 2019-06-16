660d9b0d6089c0fb680671a171395bf3557f1aec
ethanpadden <danu7_pe@danu7>
2019-06-16 20:01:42 +0100

    Restructure from testing

--- routes/account.js
+++ routes/account.js
@@ -23,18 +23,9 @@ router.get('/getBasicInfo', function(req, res ,next){
 
 router.get('/getTweetInfo', function(req, res, next){
     // Input data: { handle:string, count:int}
-    
-
-    exec('java -jar java/TweetTrack.jar tweetbytime ' + req.query.handle + ' ' + req.query.count + ' ' + 'java/statuslist.txt', function(err, stdout) {
-    /* OUTPUT CODES:
-    0 Success
-    -1 Invalid input format
-    1 Failed to get data for handle
-    2 File not found to write data
-    */
+    var javaCall = 'java -jar java/TweetTrack.jar tweetstats ' + req.query.handle + ' ' + req.query.count + ' ' + 'java/statuslist.txt';
+    exec(javaCall, function(err, stdout) {
         if(stdout)  {
-            // console.log("A::" + stdout + "::A");
-            
             setTimeout(function () {
                 fs.readFile('java/statuslist.txt', function(err, data) {
                     if(`${data}`.length == 0) {
@@ -57,72 +48,40 @@ router.get('/getTweetInfo', function(req, res, next){
                     }
                 });
             }, 1000); 
-               
-            }
-        
-       
+        } else res.json({'status':-1});
     });
-  
-
-   
-    
 });
 
 
 router.get('/getTweetsByTime', function(req, res, next){
-    
-        // Input data: { handle:string, numDays:int}
-        exec('java -jar java/TweetTrack.jar tweetbytime ' + req.query.handle + ' ' + req.query.numDays + ' ' + 'java/statuslist.txt', function(err, stdout) {
-            /* OUTPUT CODES:
-            0 Success
-            -1 Invalid input format
-            1 Failed to get data for handle
-            2 File not found to write data
-            */
-           console.log("A");
-                
-                if(err) {
-                    console.log("B");
-        
-                    res.json({"status":err.code});
-                }
-                else {
-                    console.log("C");
-
-                    if(stdout.indexOf('SUCCESS') != -1) {
-                        console.log("D");
-                    
-                        fs.readFile('java/statuslist.txt', function(err, data) {
-                            if(`${data}`.length == 0) {
-                                console.log("Data is empty");
-                                res.json({"status":-1});
-                            } else {
-                            var tweetArr = `${data}`.split('\n');
-                            var jsonArr = [];
-        
-                            for(var i = 0; i < tweetArr.length; i++) {
-                                jsonArr[i] = JSON.parse(tweetArr[i]);
-                            }
-                            console.log(jsonArr);
-                            var outputJSON = {
-                                'status':-1,
-                                'tweetStream':jsonArr
-                            };
-                            
-                            outputJSON.status = 0;
-                            res.json(outputJSON);
-                        }
-                        });
-                    } else if (stdout.indexOf('FAILURE') != -1) {
-                    console.log("E");
-
-                        console.log("CANNOT FIND FILE");
+    // Input data: { handle:string, numDays:int}
+    var javaCall = 'java -jar java/TweetTrack.jar tweetbytime ' + req.query.handle + ' ' + req.query.numDays + ' ' + 'java/statuslist.txt';
+    exec(javaCall, function(err, stdout) {
+        if(stdout)  {
+            setTimeout(function () {
+                fs.readFile('java/statuslist.txt', function(err, data) {
+                    if(`${data}`.length == 0) {
+                        console.log("Data is empty");
                         res.json({"status":-1});
+                    } else {
+                    var tweetArr = `${data}`.split('\n');
+                    var jsonArr = [];
+                    console.log("LEN: " + tweetArr.length);
+                    for(var i = 0; i < tweetArr.length-1; i++) {
+                        jsonArr[i] = JSON.parse(tweetArr[i]);
                     }
-                }
-            });
-
+                    var outputJSON = {
+                        'status':-1,
+                        'tweetStream':jsonArr
+                    };
+                    
+                    outputJSON.status = 0;
+                    res.json(outputJSON);
+                    }
+                });
+            }, 1000); 
+        }
+    });
 });
-
 
 module.exports = router;
