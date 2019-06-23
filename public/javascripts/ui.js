9ec715f094235e745b651ac0ef685115a58b6be5
ethanpadden <danu7_pe@danu7>
2019-06-23 02:12:50 +0100

    Show/hide tracker card appropriately

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -1,36 +1,38 @@
-$('#graph-tab').click(function(event){
-    $(event.target).addClass('active');
-    $('#compare-tab').removeClass('active');
-    $('#track-tab').removeClass('active');
-    showGraphs();
-});
+$('#graph-tab').click(function (event) {
+  $(event.target).addClass('active')
+  $('#compare-tab').removeClass('active')
+  $('#track-tab').removeClass('active')
+  $('#tracker').addClass('hidden')
+  showGraphs()
+})
 
- $('#compare-tab').click(function(event){
-    $(event.target).addClass('active');
-    $('#graph-tab').removeClass('active');
-    $('#track-tab').removeClass('active');
-    $('#engagement-options').removeClass('hidden');
-    hideGraphs();
-});
+$('#compare-tab').click(function (event) {
+  $(event.target).addClass('active')
+  $('#graph-tab').removeClass('active')
+  $('#track-tab').removeClass('active')
+  $('#engagement-options').removeClass('hidden')
+  $('#tracker').addClass('hidden')
+  hideGraphs()
+})
 
- $('#track-tab').click(function(event){
-    $(event.target).addClass('active');
-    $('#graph-tab').removeClass('active');
-    $('#compare-tab').removeClass('active');
-    $('#engagement-options').addClass('hidden');
-    $('#tracker').removeClass('hidden');
-    hideGraphs();
-});
+$('#track-tab').click(function (event) {
+  $(event.target).addClass('active')
+  $('#graph-tab').removeClass('active')
+  $('#compare-tab').removeClass('active')
+  $('#engagement-options').addClass('hidden')
+  $('#tracker').removeClass('hidden')
+  hideGraphs()
+})
 
-function hideGraphs() {
-    $('#likesChart').addClass('hidden');
-    $('#RTsChart').addClass('hidden');
-    $('#graph-options').addClass('hidden');
+function hideGraphs () {
+  $('#likesChart').addClass('hidden')
+  $('#RTsChart').addClass('hidden')
+  $('#graph-options').addClass('hidden')
 }
 
-function showGraphs() {
-    $('#graph-options').removeClass('hidden');
-    $('#engagement-options').addClass('hidden');
-    if ( $('#likesChart canvas').hasClass('chartjs-render-monitor'))   $('#likesChart').removeClass('hidden');
-    if ( $('#RTsChart canvas').hasClass('chartjs-render-monitor'))   $('#RTsChart').removeClass('hidden');
+function showGraphs () {
+  $('#graph-options').removeClass('hidden')
+  $('#engagement-options').addClass('hidden')
+  if ($('#likesChart canvas').hasClass('chartjs-render-monitor'))   $('#likesChart').removeClass('hidden')
+  if ($('#RTsChart canvas').hasClass('chartjs-render-monitor'))   $('#RTsChart').removeClass('hidden')
 }
