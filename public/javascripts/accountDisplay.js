1386757f30e3f39d0b6abbefbe8a168b8d3701cd
ethanpadden <danu7_pe@danu7>
2019-06-29 15:21:24 +0100

    Add tracking button to account overview

--- public/javascripts/accountDisplay.js
+++ public/javascripts/accountDisplay.js
@@ -44,7 +44,7 @@ function addUserToTable(data) {
                 + '<td>@' + data.handle + '</td>'
                 + '<td>' + data.tweetCount + '</td>'
                 + '<td>' + data.followersCount + '</td>'
-                + '<td><span class="badge badge-secondary">Not tracking</span></td>'
+                + '<td><span class="badge badge-secondary not-tracking">Not tracking</span><span class="badge badge-secondary setup-btn">Set up</span></td>'
                 + '</tr>';
                 var tableHTML = $('#overview-table-body').html();
                 tableHTML += tableRow;
