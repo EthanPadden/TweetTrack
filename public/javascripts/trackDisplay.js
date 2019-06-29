abb2e22aeeb01238b36f640beb74e745d7132306
ethanpadden <danu7_pe@danu7>
2019-06-29 16:43:34 +0100

    Update tracking status when tracker is set up

--- public/javascripts/trackDisplay.js
+++ public/javascripts/trackDisplay.js
@@ -4,34 +4,42 @@ Get day of week => get mondays date
 Make ajax request for that date
 */
 var started = false
-$('#track-tab').click(function (event) {
-  // Display current trackers
+$('#track-tab').click(function(event) {
+    // Display current trackers
 })
 
-$('#add-tracker-btn').click(function(event){
-  // Find the index of the selected option
-  var index = $('#account-dropdown-track option:selected').index();
+$('#add-tracker-btn').click(function(event) {
+    // Find the index of the selected option
+    var index = $('#account-dropdown-track option:selected').index();
 
-  // Find the handle of the selected account from array of data
-  var handle = window.accounts[index].handle;
+    // Find the handle of the selected account from array of data
+    var handle = window.accounts[index].handle;
 
-  // $.ajax({
-  //     type: 'GET',
-  //     url: '/account/getTweetInfo',
-  //     data: {'handle':handle, 'count':defaultNumTweets},
-  //     success: function(data){
-  //         if(data.status == 0) {
-  //             if(chartType == 'Likes') createLikesChart(data, handle);
-  //             else if(chartType == 'Retweets') createRTsChart(data, handle);
-              
-  //         }
-  //         else if (data.status == -1) console.log("Error");
-  //     },
-  //     error: function(errMsg) {
-  //         console.log(errMsg);
-  //     }
-  // });
+    $.ajax({
+        type: 'GET',
+        url: '/track/trackUser',
+        data: { 'handle': handle },
+        success: function(data) {
+            if (data.status == 0) {
+               updateTrackingStatus(index)
+            } else if (data.status == -1) console.log("Error");
+        },
+        error: function(errMsg) {
+            console.log(errMsg);
+        }
+    });
 });
+
+function updateTrackingStatus(i) {
+    // Keep the accounts array moving the same as the table
+    var n = i + 1
+    var tableRow = $('#overview-table-body > tr:nth-child(' + n + ')')
+    var cell = tableRow[0].cells[4].children[0]
+    $(cell).removeClass('not-tracking')
+    $(cell).addClass('tracking')
+    $(cell).html('Tracking')
+    
+}
 
 function displayTrackingInfo(dates, data) {
     $('#tracker h4').html('Week of ' + dates[0] + ' to ' + dates[1])
@@ -56,28 +64,27 @@ function displayTrackingInfo(dates, data) {
 //   return d + '/' + m + '/' + date.getFullYear()
 // }
 
-function getMonday(d)
-{
-    var prevMonday =  new Date(d.getFullYear(), d.getMonth(), d.getDate() - d.getDay() - 6) // -7+1
+function getMonday(d) {
+    var prevMonday = new Date(d.getFullYear(), d.getMonth(), d.getDate() - d.getDay() - 6) // -7+1
     var d = prevMonday.getDate().toString()
     if (d.length == 1) d = '0' + d
-    var m = (prevMonday.getMonth()+1).toString()
+    var m = (prevMonday.getMonth() + 1).toString()
     if (m.length == 1) m = '0' + m
 
     var startDate = d + '/' + m + '/' + prevMonday.getFullYear().toString()
-    
+
     var nextMonday = prevMonday;
     nextMonday.setDate(prevMonday
-      .getDate() + 7);
+        .getDate() + 7);
 
-      // If next Monday is in the future
-      var today = new Date();
-      if(nextMonday.getTime() > today.getTime()) nextMonday = today;
+    // If next Monday is in the future
+    var today = new Date();
+    if (nextMonday.getTime() > today.getTime()) nextMonday = today;
 
-      var d = nextMonday.getDate().toString()
-      if (d.length == 1) d = '0' + d
-      var m = (nextMonday.getMonth()+1).toString()
-      if (m.length == 1) m = '0' + m
-      var endDate = d + '/' + m + '/' + nextMonday.getFullYear().toString()
-      return [startDate, endDate];
-} 
+    var d = nextMonday.getDate().toString()
+    if (d.length == 1) d = '0' + d
+    var m = (nextMonday.getMonth() + 1).toString()
+    if (m.length == 1) m = '0' + m
+    var endDate = d + '/' + m + '/' + nextMonday.getFullYear().toString()
+    return [startDate, endDate];
+}
--- routes/track.js
+++ routes/track.js
@@ -4,100 +4,87 @@ const spawn = require('child_process').spawn
 const { exec } = require('child_process')
 var Tweets = require('../models/weeks')
 var fs = require('fs')
-const { fork } = require('child_process');
-var track;
-var killProcessTime = 3000;
+const { fork } = require('child_process')
+var track
+var killProcessTime = 3000
 
+router.get('/trackUser', function (req, res, next) {
+  // {handle:String}
+  var handle = req.query.handle
+  track = spawn('java', ['-jar', 'java/TweetTrack.jar', 'tracker', 'init', handle, 'mentions.txt'], { detached: true, stdio: 'ignore' })
+  track.unref() // Stops parent from waiting for tracker to exit
 
-router.get('/trackUser', function(req, res, next) {
-    // {handle:String}
-    // Steps 1 + 2
-    var handle = req.query.handle
-    track = spawn('java', ['-jar', 'java/TweetTrack.jar', 'tracker', 'init', handle, 'mentions.txt'], { detached: true, stdio: 'ignore' })
-        //  java -jar java/TweetTrack.jar tracker init elonmusk mentions.txt
+  while (true) if (track != null) break
+  var pid = track._handle.pid
 
+  fs.appendFile('proc.txt', pid + ',' + handle + '\n', (err) => {
+    if (err) console.log(err)
+  })
 
+  res.json({ 'status': 0 })
+})
 
-    track.unref(); // Stops parent from waiting for tracker to exit
+router.get('/killTracker', function (req, res, next) {
+  var handle = req.query.handle
+  var toKill = []
 
-    while (true) {
-        if (track != null) break
+  fs.readFile('proc.txt', function (err, data) {
+    var processes = `${data}`.split('\n')
+    var dataStr = ''
+    for (var i in processes) {
+      if (processes[i].indexOf(handle) != -1) {
+        var pid = processes[i].split(',')[0]
+        toKill.push(pid)
+        processes.splice(i, 1)
+      } else dataStr += processes[i] + '\n'
     }
-    var pid = track._handle.pid
-    fs.appendFile("proc.txt", pid + ',' + handle + '\n', (err) => {
-        if (err) console.log(err);
-    });
 
-    res.json({ 'tracker': 0 })
-        // // Step 6
-        // track.on('message', (msg) => {
-        //   res.json({'message':msg})
-        // });
-});
-
-router.get('/killTracker', function(req, res, next) {
-    var handle = req.query.handle
-    var toKill = []
-
-
-    fs.readFile('proc.txt', function(err, data) {
-        var processes = `${data}`.split('\n')
-        var dataStr = ""
-        for (var i in processes) {
-            if (processes[i].indexOf(handle) != -1) {
-                var pid = processes[i].split(',')[0]
-                toKill.push(pid)
-                processes.splice(i, 1)
-            } else dataStr += processes[i] + '\n'
-        }
-
-        fs.writeFile("proc.txt", dataStr, (err) => {
-            if (err) console.log(err);
-            else
-                for (var i in toKill) {
-                    exec('kill -9 ' + parseInt(toKill[i]))
-                }
-        });
-
+    fs.writeFile('proc.txt', dataStr, (err) => {
+      if (err) console.log(err)
+      else
+        for (var i in toKill) {
+          exec('kill -9 ' + parseInt(toKill[i]))
+      }
     })
-    setTimeout(function() {
-            res.json({ 'trackers_killed': toKill.length })
-        }, killProcessTime)
-        // i = 0;
-        // var killProc = spawn('kill', ['-9', parseInt(toKill[i])], { detached: false, stdio:'ignore' })
+  })
+  setTimeout(function () {
+    res.json({ 'trackers_killed': toKill.length })
+  }, killProcessTime)
+  // i = 0
+  // var killProc = spawn('kill', ['-9', parseInt(toKill[i])], { detached: false, stdio:'ignore' })
 
-    // var args = 'kill -9 ' + parseInt(toKill[0])
-    // killProc.on('exit', function (code, signal) {
-    //   console.log('child process exited with ' +
-    //               `code ${code} and signal ${signal}`);
-    // });
+  // var args = 'kill -9 ' + parseInt(toKill[0])
+  // killProc.on('exit', function (code, signal) {
+  //   console.log('child process exited with ' +
+  //               `code ${code} and signal ${signal}`)
+  // })
 
-    /*
-    // TODO - remove the ids from the file
+/*
+// TODO - remove the ids from the file
 
-      */
+  */
 })
 
-router.get('/checkStatus', function(req, res, next) {
-    // {handle:String}
-    // Steps 1 + 2
-    track.send({ cmd: 'status', handle: req.query.handle });
+router.get('/checkStatus', function (req, res, next) {
+  // {handle:String}
+  // Steps 1 + 2
+  track.send({ cmd: 'status', handle: req.query.handle })
 
-    setTimeout(function() {
-        fs.readFile('trackerstatus.txt', function(err, data) {
-            if (`${data}`.length == 0 || err) {
-                console.log("ERR")
-                if (err) console.log(err.msg)
-                res.json({ "status": -1 });
-            } else {
-                res.json({ 'data': `${data}` })
-            }
-        });
-    }, 6000)
-});
-
-process.on('exit', function() {
-    if (tracker != null) tracker.send('end')
+  setTimeout(function () {
+    fs.readFile('trackerstatus.txt', function (err, data) {
+      if (`${data}`.length == 0 || err) {
+        console.log('ERR')
+        if (err) console.log(err.msg)
+        res.json({ 'status': -1 })
+      } else {
+        res.json({ 'data': `${data}` })
+      }
+    })
+  }, 6000)
 })
 
-module.exports = router;
+process.on('exit', function () {
+  if (tracker != null) tracker.send('end')
+})
+
+module.exports = router
