c73f95f067fde112477d0996f9a2a16d742b6f5f
ethanpadden <danu7_pe@danu7>
2019-06-30 11:56:09 +0100

    Add 3 schemas for new DB system

--- models/weeks.js
+++ models/weeks.js
@@ -4,14 +4,30 @@ require('./util')
 
 var tweetsSchema = new Schema({
   tweet_id: {type: String},
+  tracker_id: {type: String},
   text: {type: String},
   created_at: {type: String},
   favourite_count: {type: Number},
   rt_count: {type: Number},
   is_rt: {type: Number},
-  week: {type: String},
   handle: {type: String},
   week: {type: String} // Weeks are identified by start date (Monday)
 })
 
+var mentionsSchema = new Schema({
+  tweet_id: {type: String},
+  handle: {type: String},
+  tracker_id: {type: String},
+  tweeting_user: {type: String},
+  created_at: {type: String},
+  text: {type: String}
+})
+var trackerSchema = new Schema({
+  start_date: {type: String},
+  end_date: {type: String},
+  handle: {type: String}
+})
+
 module.exports = mongoose.model('Tweets', tweetsSchema)
+module.exports = mongoose.model('Tracker', trackerSchema)
+module.exports = mongoose.model('Mentions', mentionsSchema)
