207d7daa4e8c58991f3a268d70a932741acd03fd
ethanpadden <danu7_pe@danu7>
2019-07-02 12:16:03 +0100

    Add pid section to trackers DB collection

--- models/trackers.js
+++ models/trackers.js
@@ -5,7 +5,8 @@ require('./util')
 var trackerSchema = new Schema({
     start_date: {type: String},
     end_date: {type: String},
-    handle: {type: String}
+    handle: {type: String},
+    pid: {type: Number}
   })
 
 module.exports = mongoose.model('Trackers', trackerSchema)
