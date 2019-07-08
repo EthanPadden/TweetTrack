56d1d9888beca48c37e3ed6baf3651c232b43f63
ethanpadden <danu7_pe@danu7>
2019-07-08 15:11:10 +0100

    Add sample data and call function to create engagement chart

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -1,4 +1,14 @@
-$(document).ready(updateStats);
+// $(document).ready(updateStats);
+var testStats = {
+
+        'tweet_count':10,
+        'likes_count':567,
+        'rt_count':324,
+        'mentions_count':1435
+}
+
+var testHandle = 'elonmusk'
+$(document).ready(updateTrackerDisplay(testStats));
 
 function updateStats() {
     // This will, in future, take an array of trackers/handles and update each one
@@ -9,6 +19,7 @@ function updateStats() {
         success: function(data) {
             if (data.status == 0) {
                console.log(data)
+               updateTrackerDisplay(data.stats)
             } else if(data.status) console.log("Error: status " + data.status);
             else console.log("Error: no status available");
         },
@@ -16,4 +27,9 @@ function updateStats() {
             console.log(errMsg);
         }
     });
+}
+
+function updateTrackerDisplay(stats) {
+    $('#tracker #last-updated').html('Last updated: ' + new Date().toString().split(' GMT')[0])
+    trackerEngmtChart(stats, testHandle)
 }
