79a253ce4fae9abb5367f9f115a70ded77caf009
ethanpadden <danu7_pe@danu7>
2019-07-08 12:06:15 +0100

    Add engagment function with stats input rather than array of tweets

--- public/javascripts/engagement.js
+++ public/javascripts/engagement.js
@@ -1,4 +1,7 @@
+import { stat } from "fs";
+
 function calculateEngagement(tweets, f) {
+    // Recheck so that the engagement is calculated using the avgs not the totals
     var l = 0;
     var r = 0;
     var t = tweets.length;
@@ -19,4 +22,17 @@ function calculateEngagement(tweets, f) {
     }
     
     return results;
+}
+
+function calculateEngagementFromStats(stats, followersCount) {
+    var avgL = stats.likes_count/stats.tweet_count
+    var avgR = stats.rt_count/stats.tweet_count
+
+    var engmt = 100(avgL/followersCount) + 1000(avgR/followersCount) + stats.mentions_count
+
+    var results = {
+        "avg_likes":avgL,
+        "avg_rts":avgR,
+        "engagement":engmt
+    }
 }
