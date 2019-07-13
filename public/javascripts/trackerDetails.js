b6f753636891549bbe320ca1f522a78b74bd8d63
ethanpadden <danu7_pe@danu7>
2019-07-13 18:34:37 +0100

    Add ajax request for gathering tweets tracked

--- public/javascripts/trackerDetails.js
+++ public/javascripts/trackerDetails.js
@@ -4,9 +4,30 @@ $("body").on('click', '#tracker-link-btn', function(){
     $(location).attr('href', '/tracker?handle=' + handle );
  });
 
- $(document).ready(setTitle)
+ $(document).ready(function(){
+    var handle = document.cookie.split('handle=')[1]
 
- function setTitle() {
-     var handle = document.cookie.split('handle=')[1]
+     setTitle(handle)
+     gatherTweets(handle)
+ })
+
+ function setTitle(handle) {
     $('#analysis-section h4').html('Analysis - ' + handle)
+ }
+
+ function gatherTweets(handle) {
+    $.ajax({
+        type: 'GET',
+        url: '/track/getTweets',
+        data: {'handle':handle},
+        success: function(data){
+            if(data.status == 0) {
+                console.log(data)
+            }
+            else if (data.status == -1) console.log("Error");
+        },
+        error: function(errMsg) {
+            console.log(errMsg);
+        }
+    });
  }
