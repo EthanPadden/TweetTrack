d594aba794f2a36edc5dd16c336c04107c1bfff4
ethanpadden <danu7_pe@danu7>
2019-07-17 15:40:36 +0100

    Add models for each collection

---
+++ models/got_users.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+require('./util')
+
+var got_usersSchema = new Schema({
+  tweet_id : {type: Number},
+  timestamp_ms : {type: String},
+  text : {type: String},
+  user_id : {type: Number},
+  quote_count : {type: Number},
+  reply_count : {type: Number},
+  retweet_count : {type: Number},
+  favourite_count : {type: Number},
+  i : {type: Number}
+})
+
+module.exports = mongoose.model('Mentions', got_usersSchema)
