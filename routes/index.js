80ac4d6b7e20dc3d0a3b3d9882d6e8b50a03d1f5
ethanpadden <danu7_pe@danu7>
2019-07-19 09:18:20 +0100

    Seperate layouts

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET overview dashboard */
 router.get('/', function(req, res, next) {
-  res.render('dash_o', { title: 'Overview' });
+  res.render('dash_o', { title: 'Overview', layout:'layout'});
 });
 
 router.get('/tracker', function(req, res, next) {
   res.cookie('handle', req.query.handle)
-  res.render('tracker', { title: 'Tracker'});
+  res.render('tracker', { title: 'Tracker', layout:'layout'});
+})
+
+router.get('/compare', function(req, res, next) {
+  res.cookie('handle', req.query.handle)
+  res.render('compare', { title: 'Game Of Thrones', layout:'layout1'});
 })
 
 module.exports = router;
