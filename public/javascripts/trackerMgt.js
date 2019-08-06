2aebe1ce31d5ba07f4c37727c503a32509861489
ethanpadden <danu7_pe@danu7>
2019-08-06 17:59:22 +0100

    Process and alert error signals from server

--- public/javascripts/trackerMgt.js
+++ public/javascripts/trackerMgt.js
@@ -1,59 +1,75 @@
-$(document).ready(function() {
-    var options = {
-        trigger:'manual'
-    }
-    $('#handle-input').tooltip(options)
+$(document).ready(function () {
+  var options = {
+    trigger: 'manual'
+  }
+  $('#handle-input').tooltip(options)
 })
 
-$('#handle-input').keyup(function(event){
-    var handle = event.target.value;
-    isHandleValid(handle);
-});
+$('#handle-input').keyup(function (event) {
+  var handle = event.target.value
+  isHandleValid(handle)
+})
 
-$('#add-account-btn').click(function(event){
-    var handle = $('#handle-input').val();
-    if (isHandleValid(handle)) {
-        $.ajax({
-            type: 'GET',
-            url: '/track/trackUser',
-            data: {'handle':handle},
-            success: function(data){
-                if(data.status == 0) {
-            //         accounts.push(data);
-            //         console.log(data)
-            // displayTrackerDetails(tracker)
-    
-            //         addUserToTable(data);
-            //         addGraphOptions();
-            //         updateOptions(data);
-            //         window.accounts = accounts;
-            //         if(accounts.length == 2) hideHandleInput();
-            //         var i = accounts.map(function(e) { return e.handle; }).indexOf(data.handle);
-            //         if(isTracking) updateTrackingStatus(i, 1)
-                    console.log(data)
-                }
-                else if (data.status == -1) $('#handle-msg').html("Account not found");
-            },
-            error: function(errMsg) {
-                console.log(errMsg);
-            }
-        });
-    } 
-});
+$('#add-account-btn').click(function (event) {
+  var handle = $('#handle-input').val()
+  if (isHandleValid(handle)) {
+    $.ajax({
+      type: 'GET',
+      url: '/track/trackUser',
+      data: {'handle': handle},
+      success: function (data) {
+        if (data.status == 0) {
+          alert('Tracker set up')
+        } else if (data.status == -1) { // Problem with Java process
+          handleErr(data.err.trim())
+        } else if (data.status == 1) { // Problem with finding tracker in DB
+          alert('There was a problem setting up the tracker in the database')
+        }
+      },
+      error: function (errMsg) {
+        console.log(errMsg)
+      }
+    })
+  }
+})
 
-function isHandleValid(handle) {
-    var specialCharReg =  /^[A-Za-z0-9_]{1,15}$/;
-    if(handle.length == 0) {
-        $('#handle-input').attr('data-original-title', 'Please enter a handle').tooltip('show');
-        return false;
-    } if(handle.length > 15) {
-        $('#handle-input').attr('data-original-title', 'Handle cannot be more than 15 characters long').tooltip('show');
-        return false;
-    } else if (!specialCharReg.test(handle)){
-        $('#handle-input').attr('data-original-title', 'Handles can only contain alphanumeric characters and underscores').tooltip('show');
-        return false;
-    }
+function handleErr (signal) {
+  switch (signal) {
+    case 'SIG_CMD_ERR_0':
+    case 'SIG_CMD_ERR_1':
+      alert('There was an error with the input arguments to the Java code')
+      break
+    case 'SIG_ACCT_ERR_0':
+      alert('There was an error when trying to access the Twitter API')
+      break
+    case 'SIG_ACCT_ERR_1':
+        console.log(signal)
+      alert('Account not found')
+      break
+    case 'SIG_DB_ERR_0':
+    case 'SIG_DB_ERR_1':
+      alert('There was an authentication problem when trying to access the database')
+      break
+    default:
+        console.log(signal)
+      alert('The server returned an error message that was not recognised')
+      break
+  }
+}
 
-    $('#handle-input').tooltip('hide');
-    return true;
- }
+function isHandleValid (handle) {
+  var specialCharReg = /^[A-Za-z0-9_]{1,15}$/
+  if (handle.length == 0) {
+    $('#handle-input').attr('data-original-title', 'Please enter a handle').tooltip('show')
+    return false
+  } if (handle.length > 15) {
+    $('#handle-input').attr('data-original-title', 'Handle cannot be more than 15 characters long').tooltip('show')
+    return false
+  } else if (!specialCharReg.test(handle)) {
+    $('#handle-input').attr('data-original-title', 'Handles can only contain alphanumeric characters and underscores').tooltip('show')
+    return false
+  }
+
+  $('#handle-input').tooltip('hide')
+  return true
+}
