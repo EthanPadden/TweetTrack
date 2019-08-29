4f5b7c489f56a5515e48e5402455111b7e9397bd
ethanpadden <danu7_pe@danu7>
2019-08-29 12:40:27 +0100

    Add getTracker function

--- public/javascripts/toolbox.js
+++ public/javascripts/toolbox.js
@@ -70,3 +70,20 @@ function createTrackerChart(trackerId, stats) {
         }
     });
 }
+
+function getTracker(trackerId) {
+    $.ajax({
+        type: 'GET',
+        url: '/track/getTracker',
+        data: {'tracker_id':trackerId},
+        success: function(data){
+            if(data.status == 0) {
+                console.log(data.tracker)
+            }
+            else console.log("Tracker not found");
+        },
+        error: function(errMsg) {
+            console.log(errMsg);
+        }
+    });
+}
