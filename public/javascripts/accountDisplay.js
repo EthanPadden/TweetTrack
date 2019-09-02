292ecf226db459b60580f2f18e6c196f3d0c33c5
ethanpadden <danu7_pe@danu7>
2019-09-02 18:20:58 +0100

    Move handle valid checker to tracker mgt and add comments

--- public/javascripts/accountDisplay.js
+++ public/javascripts/accountDisplay.js
@@ -35,3 +35,22 @@ function addUserToTable (data, trackerId, isTracking) {
   tableHTML += tableRow
   $('#overview-table-body').html(tableHTML)
 }
+
+// Check is handle valid
+function isHandleValid (handle) {
+    var specialCharReg = /^[A-Za-z0-9_]{1,15}$/
+  
+    if (handle.length == 0) {
+      $('#handle-msg').html('Please enter a handle')
+      return false
+    } if (handle.length > 15) {
+      $('#handle-msg').html('Handle cannot be more than 15 characters long')
+      return false
+    } else if (!specialCharReg.test(handle)) {
+      $('#handle-msg').html('Handles can only contain alphanumeric characters and underscores')
+      return false
+    }
+  
+    $('#handle-msg').html("Click '+' to add account")
+    return true
+  }
